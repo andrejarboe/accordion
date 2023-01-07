ff3f082e8c8a4d1eb7a217c9486a1aef22708b5b
Andre Jarboe <andrejarboe@gmail.com>
2023-01-06 21:03:45 -0500

    ass plus and minus

--- src/Question.js
+++ src/Question.js
@@ -6,7 +6,9 @@ const Question = ({ title, info }) => {
 		<article className="question">
 			<header>
 				<h4>{title}</h4>
-				<button className="btn">btn</button>
+                <button className="btn" onClick={() => setShowInfo(!isShowInfo)}>
+                    {isShowInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                </button>
 			</header>
 			{isShowInfo && <p>{info}</p>}
 		</article>
